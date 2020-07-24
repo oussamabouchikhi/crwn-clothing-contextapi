f89c3afad4311b4f76857d5bec8c8121e0547566
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-07-24 11:01:01 +0100

    Use useContext Hook with collections

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,38 +1,25 @@
-import React from 'react';
-import { connect } from 'react-redux';
-
+import React, {useContext} from 'react';
 import CollectionItem from '../../components/collection-item/collection-item.component';
-
-import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import CollectionsContext from '../../contexts/collections/collections-context';
 
 import './collection.styles.scss';
 
 const CollectionPage = ({ match }) => {
-  return (
-    <CollectionsContext.Consumer>
-      {collections => {
-        const collection = collections[match.params.collectionId];
-        const { title, items } = collection;
-        return (
-          <div className='collection-page'>
-            <h2 className='title'>{title}</h2>
-            <div className='items'>
-              {items.map(item => (
-                <CollectionItem key={item.id} item={item} />
-              ))}
-            </div>
-          </div>
-        );
-      }}
-    </CollectionsContext.Consumer>
-  )
-  
-};
+  const collections = useContext(CollectionsContext);
+  const collection = collections[match.params.collectionId];
+  const {items, title} = collection;
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
+  return (
+    <div className='collection-page'>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  )
+};
 
 export default CollectionPage;
